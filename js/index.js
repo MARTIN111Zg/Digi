9fd404aef7496b1f550648b4b3244f9ca341be82
Martin Gorupić <37804840+MARTIN111Zg@users.noreply.github.com>
2018-12-25 19:06:19 +0100

    added team meber section

--- js/index.js
+++ js/index.js
@@ -5,3 +5,18 @@
      $('#preloader').delay(1000).animate({ height: '0',opacity:'0'},"2000");
      $('#preloader').fadeOut("slow");
  });
+
+ /*--TEAM_SLIDE--*/
+ $(function(){
+    $("#team-members").owlCarousel({
+        items: 2,
+        autoplay: true,
+        smartSpeed: 700,
+        loop: true,
+        autoplayHoverPause: true,
+        nav: true,
+        dots: false,
+        navText:['<i class="fa fa-angle-left"></i>','<i class="fa fa-angle-right"></i>']
+
+    });
+  });
