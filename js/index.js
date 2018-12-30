bfe5c7d09b82f404eaae0eb79c756b14266eaaa0
Martin Gorupić <37804840+MARTIN111Zg@users.noreply.github.com>
2018-12-30 19:39:19 +0100

    added services section

--- js/index.js
+++ js/index.js
@@ -1,13 +1,15 @@
+/*preloader*/
+$(window).on('load', function () {
+    $('#status').fadeOut();
+    $('#preloader').delay(1000).animate({
+        height: '0',
+        opacity: '0'
+    }, "2000");
+    $('#preloader').fadeOut("slow");
+});
 
- /*preloader*/
- $(window).on('load',function() {
-     $('#status').fadeOut();
-     $('#preloader').delay(1000).animate({ height: '0',opacity:'0'},"2000");
-     $('#preloader').fadeOut("slow");
- });
-
- /*--TEAM_SLIDE--*/
- $(function(){
+/*--TEAM_SLIDE--*/
+$(function () {
     $("#team-members").owlCarousel({
         items: 2,
         autoplay: true,
@@ -16,19 +18,29 @@
         autoplayHoverPause: true,
         nav: true,
         dots: false,
-        navText:['<i class="fa fa-angle-left"></i>','<i class="fa fa-angle-right"></i>']
+        navText: ['<i class="fa fa-angle-left"></i>', '<i class="fa fa-angle-right"></i>']
 
     });
-  });
-  $(function() {
-    $("#progress-elements").waypoint(function() {
-        $(".progress-bar").each(function() {
+});
+$(function () {
+    $("#progress-elements").waypoint(function () {
+        $(".progress-bar").each(function () {
 
             $(this).animate({
                 width: $(this).attr("aria-valuenow") + "%"
-            },3000);
+            }, 3000);
         });
         this.destroy();
 
-    },{offset:'bottom-in-view'});
-  });
+    }, {
+        offset: 'bottom-in-view'
+    });
+});
+
+/*--RESPONSIVE_TABS--*/
+$(function () {
+
+    $("#services-tabs").responsiveTabs({
+        animation: 'slide'
+    });
+});
