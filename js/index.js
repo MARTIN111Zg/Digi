45046683119947ee4f938e3c52e34b0a567763e7
Martin Gorupić <37804840+MARTIN111Zg@users.noreply.github.com>
2019-01-01 18:28:49 +0100

    added-portf

--- js/index.js
+++ js/index.js
@@ -44,3 +44,20 @@ $(function () {
         animation: 'slide'
     });
 });
+
+/*--PORTFOLIO--*/
+$(window).on('load', function () {
+ $("#isotope-container").isotope({
+
+ });
+ $("#isotope-filters").on('click','button',function() {
+    var filterValue = $(this).attr('data-filter');
+    $("#isotope-container").isotope({ filter: filterValue });
+    $("#isotope-filters").find('.active').removeClass('active');
+    $(this).addClass('active');
+ });
+
+
+
+});
+
